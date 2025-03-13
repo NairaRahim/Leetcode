510d4a1ece976984183287d16949cb487de3cc90
NairaRahim <naira.rahim@gmail.com>
2025-03-13 04:47:21 -0700

    Time: 4 ms (94.43%) | Memory: 18.2 MB (13.52%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,12 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hashmap={}
-        for char in magazine:
-            hashmap[char]=1+hashmap.get(char,0)
-        
-        for char in ransomNote:
-            if char not in hashmap or hashmap[char]<=0:
+        if len(ransomNote)> len(magazine):
+            return False
+        for char in set(ransomNote):
+            if magazine.count(char)< ransomNote.count(char):
                 return False
-            hashmap[char]-=1
+        return True        
+        #hashmaps
+        # hashmap={}
+        # for char in magazine:
+        #     hashmap[char]=1+hashmap.get(char,0)
         
-        return True
+        # for char in ransomNote:
+        #     if char not in hashmap or hashmap[char]<=0:
+        #         return False
+        #     hashmap[char]-=1
+        
+        # return True
